07279a72760259fd453b6d64c7d3d49cd73d7db0
Sydney Bruce <sydneybruce20@gmail.com>
2024-01-11 11:00:36 -0700

    added all sections of quiz

---
+++ js/impress.js
@@ -0,0 +1,35 @@
+// impressionism questions 
+
+let questions = [
+  {question : "What are the characteristics of Impressionism?",
+    answers: [
+      {text: "Rapid brush strokes, and everyday subjects", correct: true},
+      {text: "Pastel Colors", correct: false},
+      {text: "Love of colors, animals and bright colors", correct: false},
+      {text: "Large swatches of colors and dramatic scenes", correct: false}
+    ]
+  },
+  {question : "What sparked the formation of the Impressionists?",
+  answers: [
+    {text: "A reaction to war and violence", correct: false},
+    {text: "Rebellion against the traditional style of painting", correct: true},
+    {text: "Expression of inner self", correct: true},
+    {text: "A new philosophy being pushed to artists", correct: false},
+  ]
+},
+{question : "Claude Monet is one of the most famous Impressionist painters, who commonly painted:", 
+  answers: [
+    {text: "Boat Scenes", correct: false},
+    {text: "His Garden", correct: true},
+    {text: "Ballerinas", correct: false},
+    {text: "Portraits", correct: false},
+  ]
+},
+{question: "What was the main element that Impressionist painters focused on in their paintings?",
+answers: [
+  {text: "Spitting on the canvas", correct: false},
+  {text: "Pointillism", correct: false},
+  {text: "Abstracted forms", correct: false},
+  {text: "Natural lighting", correct: true}
+]}
+];
---
+++ js/rococo.js
@@ -0,0 +1,35 @@
+// rococo period data
+
+let questions = [
+  {question : "The Rococo Period was defined not only by paintings but also by...",
+    answers: [
+      {text: "Furniture", correct: false},
+      {text: "Architecture", correct: false},
+      {text: "Fashion", correct: false},
+      {text: "All of the Above", correct: true}
+    ]
+  },
+  {question : "What is the Rococo Style generally described as?",
+  answers: [
+    {text: "Feminine and Natural", correct: true},
+    {text: "Harsh and Cold", correct: false},
+    {text: "Dramatic", correct: false},
+    {text: "Innovative and 'Avant-Garde'", correct: false},
+  ]
+},
+{question : "What were popular themes of Rococo Art?", 
+  answers: [
+    {text: "Rich people", correct: false},
+    {text: "Romance and Fun", correct: true},
+    {text: "Still Life's, Fruits, and Flowers", correct: false},
+    {text: "The Power of the Natural World", correct: false},
+  ]
+},
+{question: "One of the most famous pieces of Rococo Art, by  Jean-Honoré Fragonard is titled...",
+answers: [
+  {text: "Guernica", correct: false},
+  {text: "Las Meninas", correct: false},
+  {text: "The Swing (The Happy Accidents of the Swing)", correct: true},
+  {text: "The Garden of Earthly Delights", correct: false}
+]}
+];
